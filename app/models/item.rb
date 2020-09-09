9b63c3c0ec82e7cebd18d05ed2102b51004a74b1
am <mayumusou@gmail.com>
2020-09-09 15:34:20 +0900

    バリデーション・エラーハンドリング実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
+
+  has_one :purchase
+  belongs_to :user
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
+  has_many :purchases
+  with_options presence: true do
+    validates :nick_name
+    validates :email
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name Full-width characters"}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name Full-width characters"}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana Full-width katakana characters"}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana Full-width katakana characters"}
+    validates :birthday
+  end
+
+  validates :password, format: {with: /\A[a-zA-Z0-9]+\z/, message: 'Password Include both letters and numbers'}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
