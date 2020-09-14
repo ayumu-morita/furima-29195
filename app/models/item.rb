67b6ab1f44cb5c1338d9ea757873eeeaac8dd7e2
am <mayumusou@gmail.com>
2020-09-14 19:06:00 +0900

    カテゴリー実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :fee_burden
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :status
-  
 
   validates :name, :description, :price, :category_id, :delivery_day_id, :fee_burden_id, :prefecture_id, :status_id, :image, presence: true
 
-  validates_inclusion_of :price, in: 300..999999 
+  validates_inclusion_of :price, in: 300..999_999
 
-  validates :price, format: {with: /\A[a-zA-Z0-9]+\z/, message: 'Price Half-width number'}
+  validates :price, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Price Half-width number'}
 
   validates :category_id, numericality: { other_than: 1 }
   validates :delivery_day_id, numericality: { other_than: 1 }
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -23,57 +23,56 @@ describe Item do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it '商品名がないと出品できない' do 
+      it '商品名がないと出品できない' do
         @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
-      it 'descriptionがないと出品できない' do 
+      it 'descriptionがないと出品できない' do
         @item.description = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'category_idがないと出品できない' do 
+      it 'category_idがないと出品できない' do
         @item.category_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it 'status_idがないと出品できない' do 
+      it 'status_idがないと出品できない' do
         @item.status_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
-      it 'fee_burden_idがないと出品できない' do 
+      it 'fee_burden_idがないと出品できない' do
         @item.fee_burden_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Fee burden can't be blank")
       end
-      it 'prefecture_idがないと出品できない' do 
+      it 'prefecture_idがないと出品できない' do
         @item.prefecture_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it 'delivery_day_idがないと出品できない' do 
+      it 'delivery_day_idがないと出品できない' do
         @item.delivery_day_id = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
-      it 'price_idがないと出品できない' do 
+      it 'price_idがないと出品できない' do
         @item.price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格の範囲が、¥300以下だと出品できない      ' do 
+      it '価格の範囲が、¥300以下だと出品できない      ' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list")
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
       it '価格の範囲が、¥9,999,999以上だと出品できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list")
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
-      
+    end
   end
-end
 end
